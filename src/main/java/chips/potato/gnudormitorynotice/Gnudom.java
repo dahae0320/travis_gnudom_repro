e2e9276b893573a9cd4ed11e8abf9afc55c48f45
dahae0320 <ss7ssy@naver.com>
2020-12-03 20:43:29 +0900

    initial commit

---
+++ src/main/java/chips/potato/gnudormitorynotice/Gnudom.java
@@ -0,0 +1,13 @@
+package chips.potato.gnudormitorynotice;
+
+/**
+ * Hello world!
+ *
+ */
+public class Gnudom 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/chips/potato/gnudormitorynotice/GnudomTest.java
@@ -0,0 +1,20 @@
+package chips.potato.gnudormitorynotice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GnudomTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
